59f9178e975ec83fbc5b921f95503ea177c3d30a
Gleb <bbb155072@gmail.com>
2025-09-21 14:57:25 +0300

    create dist

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
-import PageCard from './components/pageCard.jsx'
+import PageCard from './components/PageCard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
