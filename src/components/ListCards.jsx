b31fcbb769758146445ee801842368da73593c6c
Fanty <167470470+FantyI@users.noreply.github.com>
2025-09-22 13:02:19 +0300

    Update ListCards.jsx

--- src/components/ListCards.jsx
+++ src/components/ListCards.jsx
@@ -34,7 +34,7 @@ function ListCards({ listData }) {
             className='flex w-[100%] justify-between text-main-white pt-6 pb-4 cursor-pointer'
          >
             <h2 className='text-3xl'>{listData.title}</h2>
-            <button className={`transition-all cursor-pointer ${isOpen ? 'rotate-0' : 'rotate-90'}`} ref={buttonRef}><img src="./src/icons/arrow.svg" alt="arrow" /></button>
+            <button className={`transition-all cursor-pointer ${isOpen ? 'rotate-0' : 'rotate-90'}`} ref={buttonRef}><img src="/icons/arrow.svg" alt="arrow" /></button>
          </div>
 
          <div className={`flex flex-wrap justify-between gap-6 overflow-hidden transition-all`} ref={listRef} style={{
